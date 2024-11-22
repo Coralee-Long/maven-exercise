4d0787d5ab7622f96cb0189855fca7645f52f2ca
Coralee Long <coralee.long@gmail.com>
2024-11-22 16:44:20 +0100

    add workflow dispatch

--- src/main/java/com/neuefische/Main.java
+++ src/main/java/com/neuefische/Main.java
@@ -1,8 +1,15 @@
 package com.neuefische;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+//        System.out.println("Hello, World!");
+
+        String asciiArt = FigletFont.convertOneLine("Hello, World!");
+        System.out.println(asciiArt);
     }
     public static int sum(int a, int b) {
         return a + b;
